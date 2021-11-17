c73ea688725e171fbc0a4e745f794e981eb5493c
yusuf Demir <yusuf.ferrum@gmail.com>
2021-11-17 17:38:43 +0100

    ikinci deersin yarisi

--- 1/script.js
+++ 1/script.js
@@ -18,3 +18,40 @@ for (let i = 0; i < konutKredileri.length; i++) {
 }
 document.getElementById('demo1').innerHTML = '<ul>' + list + '</ul>'
 
+//functions
+function addToCart(productName,quantity) {
+    console.log('sepete eklendi: ' + productName + ' adet:   ' + quantity)    
+}
+//addToCart()
+addToCart('limon',10)
+
+let sayHello = ()=>{
+    console.log('hello')
+}
+sayHello()
+
+let sayHello2 = function(){
+    console.log('hello2')
+}
+sayHello2()
+
+
+let product1 = {productName:'elma',unitPrice:10,quantity:5}
+function addToCart3(product) {
+    console.log('urun: ' + product.productName + ' adet: ' + product.quantity + ' fiyat: ' + product.unitPrice)
+}
+
+addToCart3(product1)
+
+let product2 = {productName:'elma',unitPrice:10,quantity:5}
+let product3 = {productName:'elma',unitPrice:10,quantity:5}
+product2 = product3
+product2.productName='Karpuz'
+console.log(product3.productName)
+//
+
+let sayi1 = 10
+let sayi2 = 20
+sayi1=sayi2
+sayi2=100
+console.log(sayi1)
