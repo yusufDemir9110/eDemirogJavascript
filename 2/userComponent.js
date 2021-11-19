56c0ca86b1f0c233b10454ffb69d3f39dfa528cb
yusuf Demir <yusuf.ferrum@gmail.com>
2021-11-19 18:25:05 +0100

    ucuncu ders sonu

---
+++ 2/userComponent.js
@@ -0,0 +1,28 @@
+import User from "./user.js"
+import UserService from "./userServices.js"
+import {BaseLogger, ElasticLogger, MongoLogger} from "./logger.js"
+
+console.log("user component yuklendi")
+
+let logger1 = new MongoLogger()
+let userService = new UserService(logger1)
+
+let user1 = new User(1,"yusuf","demir","grave")
+let user2 = new User(2,"yavuz","demir","grave")
+
+userService.add(user1)
+userService.add(user2)
+//kullanicilari kaydettik
+
+console.log(userService.list())
+//kullanicilari listeledik
+
+console.log(userService.getById(2))
+//kullanicilari id ye gore getirdik
+
+
+//EK BILGI
+//prototyping yani sonradan deger ekleme
+let customer = {id:1,firstName:'Yusuf'}
+customer.lastName='demir'
+console.log(customer.lastName)
